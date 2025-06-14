02070f0d1383af607954874d108817518818c72c
AndriiButenko05 <161694214+AndriiButenko05@users.noreply.github.com>
2025-06-14 12:40:45 +0000

    Deploying to gh-pages from @ AndriiButenko05/goit-js-hw-12@82835a4b1f641a3d9ae56b1b0d37dc44c65d88ab 🚀

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-import{S as m,a as p,i as c}from"./assets/vendor-CrlV4O_2.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const a of t.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&i(a)}).observe(document,{childList:!0,subtree:!0});function o(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function i(e){if(e.ep)return;e.ep=!0;const t=o(e);fetch(e.href,t)}})();const d=document.querySelector(".gallery"),u=document.querySelector(".loader");function h(s){return s.map(({largeImageURL:r,webformatURL:o,tags:i,likes:e,views:t,comments:a,downloads:f})=>`<li class="gallery-item">
-        <a class="gallery-link" href="${r}">
+import{S as $,a as q,i as l}from"./assets/vendor-CrlV4O_2.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const o of e)if(o.type==="childList")for(const c of o.addedNodes)c.tagName==="LINK"&&c.rel==="modulepreload"&&i(c)}).observe(document,{childList:!0,subtree:!0});function r(e){const o={};return e.integrity&&(o.integrity=e.integrity),e.referrerPolicy&&(o.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?o.credentials="include":e.crossOrigin==="anonymous"?o.credentials="omit":o.credentials="same-origin",o}function i(e){if(e.ep)return;e.ep=!0;const o=r(e);fetch(e.href,o)}})();const d=document.querySelector(".gallery"),m=document.querySelector(".loader"),u=document.querySelector(".load-btn");function g(s){return s.map(({largeImageURL:t,webformatURL:r,tags:i,likes:e,views:o,comments:c,downloads:x})=>`<li class="gallery-item">
+        <a class="gallery-link" href="${t}">
         <img
       class="gallery-image"
-      src="${o}"
-      data-source="${r}"
+      src="${r}"
+      data-source="${t}"
       alt="${i}"
     />
   </a>
@@ -14,16 +14,16 @@ import{S as m,a as p,i as c}from"./assets/vendor-CrlV4O_2.js";(function(){const 
   </div>
   <div class="container-for-desc">
   <h3 class="img-heading">Views</h3>
-  <p class="img-text">${t}</p>
+  <p class="img-text">${o}</p>
   </div>
   <div class="container-for-desc">
   <h3 class="img-heading">Comments</h3>
-  <p class="img-text">${a}</p>
+  <p class="img-text">${c}</p>
   </div>
   <div class="container-for-desc">
   <h3 class="img-heading">Downloads</h3>
-  <p class="img-text">${f}</p>
+  <p class="img-text">${x}</p>
   </div>
   </div>
-</li>`).join("")}function g(){d.innerHTML=""}function y(){u.style.display="block"}function v(){u.style.display="none"}const b=new m(".gallery a",{captionsData:"alt",captionDelay:250}),L="50744753-f362bf6174861c1f1e53027f5";function w(s){y();const r=`"${s}"`,o=encodeURIComponent(r);return p.get(`https://pixabay.com/api/?key=${L}&q=${o}&image_type=photo&orientation=horizontal&safesearch=true`)}const n=document.querySelector('[name="search-text"]'),l=document.querySelector(".form");n.addEventListener("keydown",()=>{n.style.borderColor="#4E75FF"});n.addEventListener("blur",()=>{n.style.borderColor="#808080"});l.addEventListener("submit",s=>{if(s.preventDefault(),n.value.trim()===""){c.show({message:"Please, write correct name of photo",position:"topRight",color:"red"});return}g(),w(n.value.trim()).then(r=>{const o=r.data.hits;if(o.length===0){c.show({message:"Sorry, there are no images matching your search query. Please try again!",position:"topRight",color:"red"});return}const i=h(o);d.insertAdjacentHTML("beforeend",i),b.refresh()}).catch(r=>{c.show({message:`Sorry, something went wrong: ${r}`,position:"topRight",color:"red"})}).finally(()=>{v()}),l.reset()});
+</li>`).join("")}function P(){d.innerHTML=""}function E(){m.style.display="block"}function f(){m.style.display="none"}const p=new $(".gallery a",{captionsData:"alt",captionDelay:250});function M(){u.style.display="block"}function y(){u.style.display="none"}const B="50744753-f362bf6174861c1f1e53027f5",v=15;async function w(s,t){try{E();const r=new URLSearchParams({key:B,q:s,page:t,per_page:v,image_type:"photo",orientation:"horizontal",safesearch:!0});return q.get(`https://pixabay.com/api/?${r}`)}catch(r){console.log("Error:",r)}}const a=document.querySelector('[name="search-text"]'),h=document.querySelector(".form");let b=0,L,n=1,S;a.addEventListener("keydown",()=>{a.style.borderColor="#4E75FF"});a.addEventListener("blur",()=>{a.style.borderColor="#808080"});h.addEventListener("submit",s=>{if(s.preventDefault(),a.value.trim()===""){l.show({message:"Please, write correct name of photo",position:"topRight",color:"red"});return}n=1,y(),P(),w(a.value.trim(),n).then(t=>{const r=t.data.hits;if(r.length===0){l.show({message:"Sorry, there are no images matching your search query. Please try again!",position:"topRight",color:"red"});return}const i=g(r);d.insertAdjacentHTML("beforeend",i),b=document.querySelector(".gallery-item").getBoundingClientRect().height,S=Math.ceil(t.data.totalHits/v),M(),p.refresh()}).catch(t=>{l.show({message:`Sorry, something went wrong: ${t}`,position:"topRight",color:"red"})}).finally(()=>{f(),n++}),L=a.value.trim(),h.reset(),console.log(n)});u.addEventListener("click",()=>{n>S?(y(),l.show({message:"We're sorry, but you've reached the end of search results.",position:"topRight",color:"blue"})):w(L,n).then(s=>{const t=s.data.hits,r=g(t);d.insertAdjacentHTML("beforeend",r),window.scrollBy({top:b,behavior:"smooth"}),p.refresh()}).catch(s=>{l.show({message:`Sorry, something went wrong: ${s}`,position:"topRight",color:"red"})}).finally(()=>{f(),n++})});
 //# sourceMappingURL=index.js.map
